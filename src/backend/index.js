c9582fb025079d8897e9da807e7fc53701ac0e78
Sascha <sascha.scholz@rzscholz.de>
2019-01-30 14:51:31 +0100

    edit gitignore

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,44 +1,41 @@
 import Router from 'koa-router'
 
-const render = require('./render');
-const logger = require('koa-logger');
-const koaBody = require('koa-body');
-const serve   = require('koa-static');
-const send    = require('koa-send');
+const render    = require('./render');
+const logger    = require('koa-logger');
+const koaBody   = require('koa-body');
+const serve     = require('koa-static');
+const Koa       = require('koa');
+const livereload = require('koa-livereload');
 
-const Koa = require('koa');
 const router = new Router()
 const app = module.exports = new Koa();
 
-// "database"
-
+// "database" from example
 const posts = [];
 
 // middleware
-
+app.use(livereload());
 app.use(logger());
 app.use(serve('./public'));
 app.use(render);
 app.use(koaBody());
 
-
-
-
+//defining the endpoints
 router.get('/index', async function (ctx) {
         await ctx.render('index');
-    })
+    });
 router.get('/', async function (ctx) {
         await ctx.render('list');
-    })
+    });
 router.get('/post/new', async function add(ctx) {
         await ctx.render('new');
-    })
+    });
 router.get('/post/:id', async function show(ctx) {
         const id = ctx.params.id;
         const post = posts[id];
         if (!post) ctx.throw(404, 'invalid post id');
         await ctx.render('show', { post: post });
-    })
+    });
 router.post('/post', async function create(ctx) {
         const post = ctx.request.body;
         const id = posts.push(post) - 1;
@@ -51,5 +48,4 @@ app.use(router.routes());
 
 // listen
 app.listen(3000);
-
 console.log("listening to port 3000");
